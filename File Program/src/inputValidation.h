3b3b0ec58be91f71e30e67bbfea0708ef6c0d141
Devtaaa <113565766+devtadanarsa@users.noreply.github.com>
2022-12-10 08:14:23 +0800

    Update inputValidation.h

--- File Program/src/inputValidation.h
+++ File Program/src/inputValidation.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <string.h>
 
 int validInt(int *var){
 	char buff[1024];
@@ -45,4 +47,70 @@ void reVldFloat(float *var, char *prompt){
 			printf("\n");
         }    
 	}
+}
+
+ssize_t getpasswd (char **pw, size_t sz, int mask, FILE *fp)
+{
+    if (!pw || !sz || !fp) return -1;
+    #ifdef MAXPW
+        if (sz > MAXPW) sz = MAXPW;
+    #endif
+
+    if (*pw == NULL){
+        void *tmp = realloc (*pw, sz * sizeof **pw);
+        if (!tmp)
+            return -1;
+        memset (tmp, 0, sz);
+        *pw =  (char*) tmp;
+    }
+
+    size_t idx = 0;
+    int c = 0;
+
+    struct termios old_kbd_mode;
+    struct termios new_kbd_mode;
+
+    if (tcgetattr (0, &old_kbd_mode)) {
+        fprintf (stderr, "%s() error: tcgetattr failed.\n", __func__);
+        return -1;
+    } 
+    memcpy (&new_kbd_mode, &old_kbd_mode, sizeof(struct termios));
+
+    new_kbd_mode.c_lflag &= ~(ICANON | ECHO);
+    new_kbd_mode.c_cc[VTIME] = 0;
+    new_kbd_mode.c_cc[VMIN] = 1;
+    if (tcsetattr (0, TCSANOW, &new_kbd_mode)) {
+        fprintf (stderr, "%s() error: tcsetattr failed.\n", __func__);
+        return -1;
+    }
+
+    while (((c = fgetc (fp)) != '\n' && c != EOF && idx < sz - 1) ||
+            (idx == sz - 1 && c == 127))
+    {
+        if (c != 127) {
+            if (31 < mask && mask < 127) 
+                fputc (mask, stdout);
+            (*pw)[idx++] = c;
+        }
+        else if (idx > 0) { 
+            if (31 < mask && mask < 127) {
+                fputc (0x8, stdout);
+                fputc (' ', stdout);
+                fputc (0x8, stdout);
+            }
+            (*pw)[--idx] = 0;
+        }
+    }
+    (*pw)[idx] = 0; 
+
+    if (tcsetattr (0, TCSANOW, &old_kbd_mode)) {
+        fprintf (stderr, "%s() error: tcsetattr failed.\n", __func__);
+        return -1;
+    }
+
+    if (idx == sz - 1 && c != '\n') 
+        fprintf (stderr, " (%s() warning: truncated at %zu chars.)\n",
+                __func__, sz - 1);
+
+    return idx; 
 }
