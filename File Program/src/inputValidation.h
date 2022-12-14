63215e2e6a08902689587ff60f386a20a92a0368
Devtaaa <113565766+devtadanarsa@users.noreply.github.com>
2022-12-14 18:23:29 +0800

    Update inputValidation.h

--- File Program/src/inputValidation.h
+++ File Program/src/inputValidation.h
@@ -3,52 +3,6 @@
 #include <termios.h>
 #include <string.h>
 
-int validInt(int *var){
-	char buff[1024];
-	char cek;
-	fflush(stdin);
-	if(fgets(buff, sizeof(buff), stdin) != NULL){
-		if(sscanf(buff, "%d %c", var, &cek) == 1) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void reVldInt(int *var, char *prompt){
-	while(1){
-		printf(prompt);
-		if(validInt(var)){
-			break;
-			printf("Input hanya berupa angka!! \n");
-			printf("\n");
-        }    
-	}
-}
-
-int validFloat(float *var){
-	char buff[1024];
-	char cek;
-	fflush(stdin);
-	if(fgets(buff, sizeof(buff), stdin) != NULL){
-		if(sscanf(buff, "%f %c", var, &cek) == 1) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void reVldFloat(float *var, char *prompt){
-	while(1){
-		printf(prompt);
-		if(validFloat(var)){
-			break;
-			printf("Input hanya berupa angka!! \n");
-			printf("\n");
-        }    
-	}
-}
-
 ssize_t getpasswd (char **pw, size_t sz, int mask, FILE *fp)
 {
     if (!pw || !sz || !fp) return -1;
