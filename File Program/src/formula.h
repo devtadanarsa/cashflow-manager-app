925fb0e5122e6eaa96c4cd4d0631d792c1270819
Gilbert SM <113496145+gilbertmulyadi@users.noreply.github.com>
2022-12-26 13:15:36 +0700

    tambah komentar

--- File Program/src/formula.h
+++ File Program/src/formula.h
@@ -1,19 +1,50 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//Fungsi Untuk Menghitung Persentase
+//Input Berupa Integer Sedangkan Output Berupa Float!
 float persentase(int a, int b){
     float persentase = (float)a/(float)b * 100;
     return persentase;
 }
 
+//Fungsi Untuk Melakukan Sorting
+//Menggunakan Metode "Bubble Sort"
 void bubbleSort(int arr[], int arrPenanda[]){
     for(int i = 0; i < 7; i++){
         for(int j = 0; j < 7; j++){
             if(arr[j] < arr[j+1]){
+                //Sorting Array Asli
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                //Sorting Array Tiruan
                 int tempPen = arrPenanda[j];
                 arrPenanda[j] = arrPenanda[j+1];
                 arrPenanda[j+1] = tempPen;
             }
         }
     }
+}
+
+//Fungsi Untuk Menghitung Banyaknya Baris
+//Pada File account.txt
+int checkLines(){
+    FILE *fptr;
+    char ch;
+    int lines = 1;
+    fptr = fopen("database/account.txt", "r");
+    if(fptr == NULL){
+        printf("Unable to open file");
+    }
+    do{
+        ch = fgetc(fptr);
+        if(ch == '\n'){
+            lines++;
+        }
+    }while(ch != EOF);
+
+    fclose(fptr);
+    return lines;
 }
